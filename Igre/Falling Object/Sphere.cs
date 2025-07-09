5a06c5baa4a635f8fe07a204de8a3e14064b7558
Shadowh86 <78486817+Shadowh86@users.noreply.github.com>
2025-07-09 08:58:39 +0200

    Create Sphere.cs

---
+++ Igre/Falling Object/Sphere.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+    [SerializeField] private int scoreAmount;
+    
+   
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.TryGetComponent(out PlayerMovement playerMovement))
+        {
+            playerMovement.IncreaseScore(scoreAmount);
+        }
+            Destroy(gameObject);
+    }
+}
